ff661342f6790ca8bf814dc12c7efaee9818e8fe
KacperB <kacpero.barszczewski@gmail.com>
2025-05-08 00:37:36 +0200

    fix: update publicPath in webpack configuration to fix deploy on gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
-    publicPath: "/",
+    publicPath: "/webdev/",
   },
   resolve: {
     extensions: [".js", ".jsx"],
