43a359942fcc3a5206ccc9b8a2a153ade1950281
KacperB <kacpero.barszczewski@gmail.com>
2025-05-08 22:19:52 +0200

    feat: refactor Navbar component structure and styles for improved layout and responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,12 +6,22 @@ import hamburgerImg from '../../assets/images/icon-hamburger.svg';
 const NavBar = () => {
     return (
         <nav className="navbar">
-            <div>
-                <img src={logoImg} alt="Logo" className="navbar__logo" />
+            <div className="navbar__container">
+
+                <div>
+                    <img src={logoImg} alt="Logo" className="navbar__container__logo" />
+                </div>
+                <div>
+                    <img src={hamburgerImg} alt='hamburger' className='navbar__container__hamburger' />
+                    <div className='navbar__container__links'>
+                        <a href="#features" className='navbar__link'>Features</a>
+                        <a href="#pricing" className='navbar__link'>Pricing</a>
+                        <a href="#contact" className='navbar__link'>Contact</a>
+                        <button>Login</button>
+                    </div>
+                </div>
             </div>
-            <div>
-                <img src={hamburgerImg} alt='hamburger' className='navbar__hamburger' />
-            </div>
+
         </nav>
     );
 };
