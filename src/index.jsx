f4ca0175b08d5b81ea291b83e71b774647c5f477
KacperB <kacpero.barszczewski@gmail.com>
2025-05-08 02:51:36 +0200

    feat: refactor App component and update stylesheets

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./style.scss";
+import "./styles/main.scss";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<React.StrictMode><App /></React.StrictMode>);
